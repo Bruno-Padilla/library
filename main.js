3bbebc884f7e1356b30cd09cc4d4bdca805d199c
Brunniel <brunisdan5@gmail.com>
2026-05-20 21:28:48 -0600

    form layout complete

---
+++ main.js
@@ -0,0 +1,12 @@
+/* - - - - - HTML ELEMENTS - - - - - */
+let btnAddBook = document.querySelector(".btn-addBook");
+
+
+/* - - - - - EVENTS - - - - - */
+btnAddBook.onclick = displayForm;
+
+
+/* - - - - - FUNCTIONS - - - - - */
+function displayForm() {
+    alert("lolo");
+}
